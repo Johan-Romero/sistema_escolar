4e4aa7d0b77bbe11fbf083fd07eed38533161d83
Johan-Romero <johanlromero2001@gmail.com>
2025-07-08 20:09:05 -0500

    base de datos

--- backend/settings.py
+++ backend/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sistema_gestion',
+        'NAME': 'sistema_escolar1',
         'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST': 'localhost',
